3406963d3af253c1c9d6da59aafc34a7193cad51
PRASANNA <PRASANNA@LAPTOP-I0I59B7L>
2025-02-24 00:31:09 +0530

    hide creds

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://Prasanna:Password%40123@mongolearn.ytg3r.mongodb.net/DevTinder"
+    "sd"
   );
 };
 
