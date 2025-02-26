9ec6cbedd160b5f3f7b54d73a744a083a3f86c6c
PRASANNA <PRASANNA@LAPTOP-I0I59B7L>
2025-02-26 22:01:54 +0530

    data update delete users

--- src/app.js
+++ src/app.js
@@ -1,14 +1,81 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
+const user = require("./models/user");
 
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const user = await User.find({ email: userEmail });
+
+    if (user.length == 0) {
+      res.send(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (error) {
+    res.send(500).send("Something went wrong" + error);
+  }
+});
+
+app.delete("/deleteUser", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    res.send("user is successfully deleted");
+  } catch (error) {
+    res.send(500).send("Something went wrong" + error);
+  }
+});
+
+app.patch("/updateUser", async (req, res) => {
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: data.userId }, data, {
+      returnDocument: "after",
+    });
+    console.log(user);
+    res.send("Updated data successfully");
+  } catch (error) {
+    res.send(500).send("Something went wrong" + error);
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  /* const user = new User({
+    firstName:"Prasanna",
+    lastName:"Kumar",
+    email:"prasanna@gmail.com",
+    password:"password",
+    age:20,
+    gender:"Male"
+}) */
+  const user = new User(req.body);
+  await user
+    .save()
+    .then((Result) => {
+      res.send({
+        message: "Data inserted successfully",
+        data: Result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
 
 connectDB()
   .then(() => {
     console.log("Database connected");
     app.listen(3000, () => {
-        console.log("Server is running on port 3000");
-      });
+      console.log("Server is running on port 3000");
+    });
   })
   .catch((err) => {
     console.log("Error in connecting to database", err);
@@ -42,7 +109,3 @@ app.use("/test1", (req, res,next) => {
 app.use("/test/1", (req, res) => {
   res.send("Welcome to Dev tinder1 test");
 }); */
-
-
-
-
